b088116eba94f2dbe37e98cb2b2f0365faaa4e34
abdusattormv <mamatovsattor12@gmail.com>
2025-04-24 02:34:43 +0900

    remove node fetch

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const { Telegraf, session } = require("telegraf");
-const fetch = require("node-fetch");
 const FormData = require("form-data");
 const bodyParser = require("body-parser");
 
